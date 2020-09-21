7aae170b1c5e23f154510ff545d0f5429b51bdb0
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-21 12:57:06 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+
 const NavBar = () => {
   return (
     <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
